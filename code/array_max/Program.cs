988cf84cb115853b1b13dfd737faa8916d3be89c
TimofeyMeshcheryakov <tdmesherykov@mail.ru>
2026-03-13 16:54:11 +0300

    Step 7 arrays

---
+++ code/array_max/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("введите N ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+int max = array[0];
+for (int i = 0; i < n; i++)
+{
+    array[i] = i + 1;
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"{i}: {array[i]}");
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+}
+Console.WriteLine(max);
---
+++ code/print_array/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите N ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    array[i] = i + 1;
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"{i}: {array[i]}");
+}
